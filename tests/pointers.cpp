98596cdd81703e2353f23d5a0b67786935b92b61
Felipe Campelo <97348832+feed0@users.noreply.github.com>
2024-04-16 15:02:38 +0000

    added comments to the file pointers.cpp

--- tests/pointers.cpp
+++ tests/pointers.cpp
@@ -6,9 +6,13 @@ using namespace std;
 int main() {
     int a, *b;
 
-    a = 10;     cout << a << "   " << &a << "    " << &a + 2        << endl;
+    // a {&: 0x &a, value:    int}
+    // b {&: 0x &b, value: &  int}
+
+    // &a + 2 =     &a + 2 * SIZEOF(int) =      &a + 2 * 4 = &a + 8
+    a = 10;     cout << a << "   " << &a             << "    " << &a + 2        << endl;
     b = &a;     cout << b << "   " << (*b) * 2       << endl;
-    *b = 13;    cout << a << "   " << &b + 2        << endl;
+    *b = 13;    cout << a << "   " << &b + 2         << endl;
 
     return 0;
 }
